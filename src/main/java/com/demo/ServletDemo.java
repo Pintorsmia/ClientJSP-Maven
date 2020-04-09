2d992ded8fdc13d8b3c4011939cca3887f132e32
Janin Damien <damien.janin@soprasteria.com>
2020-04-09 11:46:24 +0200

    Correction ServletDemo.java

--- src/main/java/com/demo/ServletDemo.java
+++ src/main/java/com/demo/ServletDemo.java
@@ -17,6 +17,6 @@ public class ServletDemo extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
-        out.println("<h3>Hello World!</h3>");
+        out.println("<h3>Servlet Test!</h3>");
     }
 }
