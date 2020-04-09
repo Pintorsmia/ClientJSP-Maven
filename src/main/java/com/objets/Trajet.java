76b67917e44286ec57e03a9e2da936fa65ff1c76
Janin Damien <damien.janin@soprasteria.com>
2020-04-09 13:50:26 +0200

    test Pour soap + changement dans le json

---
+++ src/main/java/com/objets/Trajet.java
@@ -0,0 +1,33 @@
+package com.objets;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Trajet {
+    private String DateDepart,DateArrive,Vehicule;
+    private int Temps;
+
+    public Trajet(String dateDepart, String dateArrive, int temps, String vehicule) throws ParseException {
+        DateDepart = this.dateConvert(dateDepart);
+        DateArrive =  this.dateConvert(dateArrive);
+        Temps = temps;
+        Vehicule = vehicule;
+    }
+
+    private String dateConvert (String dateInString) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+        Date date = formatter.parse(dateInString);
+        String result = new SimpleDateFormat("dd/MM/yyyy HH:mm").format(date);;
+        return result;
+    }
+    @Override
+    public String toString() {
+        return  "Heure de départ :'" + DateDepart + '\'' +
+                ", Heure d'arrivé :'" + DateArrive + '\'' +
+                ", Durée du com.objets.Trajet :'" + Temps + '\'' +
+                ", Moyen de transport :'" + Vehicule + '\'';
+    }
+}
+
+
